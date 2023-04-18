fc492d7c6cb15a1fb42e3494625ea95c7c9ab055
Lollopie <florianmartin.piel@campus.tu-berlin.de>
2023-04-18 22:52:39 +0200

    Added first simple registration, login and home page

---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let app = express();
+let cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.get('/', (req, res) => {
+    res.sendFile('login.html', {root: __dirname})
+});
+app.get('/register', (req, res) => {
+    if(req.query.username && req.query.password) {
+        //Handle registration
+        res.redirect('/login?username=' + req.query.username + '&password=' + req.query.password)
+    }else{
+        res.sendFile('register.html', {root: __dirname})
+    }
+});
+app.get('/login*', (req, res) => {
+    console.log(req.query);
+    // res.send('Validating login...');
+    setTimeout(() => {
+        res.redirect('/home');
+    }, 2000);
+});
+app.get('/home', (req, res) => {
+    res.sendFile('home.html', {root: __dirname})
+});
+app.listen(3000, () => {
+
+});
