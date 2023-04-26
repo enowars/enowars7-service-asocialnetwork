605a77aaea37e5eb1ac5c774081487adcc183d15
Lollopie <florianmartin.piel@campus.tu-berlin.de>
2023-04-27 00:15:09 +0200

    Added some css to make the site viewable, and made private messages work in practice

--- routers/messageRouter.js
+++ routers/messageRouter.js
@@ -3,7 +3,55 @@ const router = express.Router();
 const User = require('../models/user');
 const Message = require('../models/message');
 const {query} = require("express");
+async function displayMessages(req, res, param){
+    if(req.cookies.session === undefined) {
+        res.redirect('/login');
+        return;
+    }
+    let user = await User.findOne().bySession(req.cookies.session);
+    if(user.length === 0) {
+        res.redirect('/register');
+        return;
+    }
+    user = user[0];
+    const messages = await Message.find({ $or: [{ sender: user._id }, { recipient: user._id }] });
+    let filteredMessages = [];
+    for(const message of messages) {
+        let newMessage =
+            {
+                message:
+                    {
+                        message:     message.message,
+                        createdAt:   message.createdAt.toLocaleTimeString() + ' ' + message.createdAt.toLocaleDateString(),
+                        sender:      await getUserNameById(message.sender),
+                        recipient:   await getUserNameById(message.recipient)
+                    }
+            };
+        let recipient = (await User.findById(message.recipient)).userName;
+        if(user.userName === recipient) {
+            newMessage.partner = await getUserNameById(message.sender);
+        }
+        else{
+            newMessage.partner = recipient;
+        }
+        filteredMessages.push(newMessage);
+    }
+    let groupedMessages = [];
+    let partners = [...new Set(filteredMessages.map((message) => {
+        return message.partner;
+    }))];
+    partners.forEach((partner) => {
+        let messages = [];
+        filteredMessages.forEach((message) => {
+            if(message.partner === partner) {
+                messages.push(message.message);
+            }
+        });
+        groupedMessages.push({partner: partner, messages: messages});
+    });
 
+    res.render('messages', { messages : groupedMessages, userName: await getUserNameById(user._id), ... param});
+}
 router.post('/', async (req, res) => {
    if(req.cookies.session === undefined) {
         res.redirect('/login');
@@ -15,16 +63,16 @@ router.post('/', async (req, res) => {
         return;
    }
    if(req.body.message === '') {
-        res.render('messages', {error: 'Please enter a message'});
+        await displayMessages(req, res, {error: 'Please enter a message'});
         return;
    }
    let recipient = await User.findOne().byUserName(req.body.recipient);
    if(recipient.length === 0) {
-      res.render('messages', {error: 'Recipient does not exist'});
-         return;
+        await displayMessages(req, res, {error: 'Recipient does not exist'});
+        return;
    }
    if(sender[0].userName === recipient[0].userName) {
-        res.render('messages', {error: 'You cannot send a message to yourself'});
+        await displayMessages(req, res, {error: 'You cannot send a message to yourself'});
         return;
    }
     let message = new Message({
@@ -32,8 +80,8 @@ router.post('/', async (req, res) => {
         recipient: recipient[0]._id,
         message: req.body.message
     });
-    message.save().then(() => {
-       res.send(message);
+    message.save().then(async () => {
+       await displayMessages(req, res, {});
     });
 
 });
@@ -42,51 +90,6 @@ async function getUserNameById(userId) {
     return user.userName;
 }
 router.get('/', async (req, res) => {
-    if(req.cookies.session === undefined) {
-        res.redirect('/login');
-        return;
-    }
-   let user = await User.findOne().bySession(req.cookies.session);
-   if(user.length === 0) {
-        res.redirect('/register');
-        return;
-   }
-   user = user[0];
-   const messages = await Message.find({ $or: [{ sender: user._id }, { recipient: user._id }] });
-   let filteredMessages = [];
-   for(const message of messages) {
-       let newMessage =
-           {
-               message:
-               {
-                   message:     message.message,
-                   createdAt:   message.createdAt.toLocaleTimeString() + ' ' + message.createdAt.toLocaleDateString(),
-                   sender:      await getUserNameById(message.sender),
-                   recipient:   await getUserNameById(message.recipient)
-               }
-           };
-       if(await User.findOne().bySession(req.cookies.session) === await User.findById(message.recipient)) {
-           newMessage.partner = await getUserNameById(message.sender);
-       }
-       else{
-           newMessage.partner = await getUserNameById(message.recipient);
-       }
-       filteredMessages.push(newMessage);
-   }
-   let groupedMessages = [];
-   let partners = [...new Set(filteredMessages.map((message) => {
-        return message.partner;
-   }))];
-   partners.forEach((partner) => {
-      let messages = [];
-      filteredMessages.forEach((message) => {
-         if(message.partner === partner) {
-            messages.push(message.message);
-         }
-      });
-      groupedMessages.push({partner: partner, messages: messages});
-   });
-
-   res.render('messages', { messages : groupedMessages, userName: await getUserNameById(user._id) });
+    await displayMessages(req, res, {});
 });
 module.exports = router;
